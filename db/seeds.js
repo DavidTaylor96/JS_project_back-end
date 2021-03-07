705f639bf90b05730ab95f982fd88bfee791dd79
JamesSBeedle <jamessbeedle@gmail.com>
2021-03-07 14:30:18 +0000

    values added to seed file

--- db/seeds.js
+++ db/seeds.js
@@ -2,6 +2,29 @@ use tryAtomic;
 
 db.dropDatabase();
 
+
+db.emissionFactors.insertMany([
+    {
+        transport: {
+            car: "0.19",
+            bus: "0.16",
+            train: "0.14",
+            plane: "0.28",
+        },
+        food:{
+            highMeat: "7.19",
+            mediumMeat: "5.63",
+            lowMeat: "4.67",
+            pescatarian: "3.91",
+            vegetarian: "3.81",
+            vegan: "2.89",
+        },
+        energy:{
+            electricity: "0.309",
+            gas: "0.203",
+        }
+    }
+])
 db.userData.insertMany([
     {
         bus:1000,
