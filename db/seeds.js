9d9dad13cd48f7b49677c19b75f1004cf3a32dd5
DavidTaylor96 <david_taylor96@outlook.com>
2021-03-10 09:41:07 +0000

    fixing the delete button

--- db/seeds.js
+++ db/seeds.js
@@ -27,21 +27,16 @@ db.emissionFactors.insertMany([
 ])
 db.userData.insertMany([
     {
-        bus:1000,
         electricity: 50,
         gas: 20, 
-        highMeat: 7.19
         
     },
     {
         car: 500,
-        electricity: 250,
-        gas: 50,
-        vegan: 2.89,
+        bus: 100,
+        plane: 800,
     },
     {
-        electricity: 50,
-        gas: 20,
         lowMeat: 4.67,
     },
 ])
